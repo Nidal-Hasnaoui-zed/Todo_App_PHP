e53b6944a00691ab44d5d1a8b6098aaec50d2551
Hairo <hasnaouinidal308@gmail.com>
2025-07-17 20:44:34 +0100

    the work version of this project

--- action files/conx.php
+++ action files/conx.php
@@ -1,12 +1,13 @@
 <?php 
     $server_name = 'localhost';
-    $db_name = ' todo_list_app'; 
-    $user_name = 'root'; 
-    $password = ''; 
-  try {
-    $conn = new PDO("mysql:host=$server_name;dbname=$db_name", $user_name, $password); 
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
-  }  catch (PDOException $e){
-        die('sorry but you have some probelm with db ! like this one : <br>'. $e->getMessage()); 
-  }
+    $db_name = 'todo_list_app'; // تم حذف المسافة الزائدة
+    $user_name = 'root';
+    $password = '';
+
+    try {
+        $conn = new PDO("mysql:host=$server_name;dbname=$db_name;charset=utf8", $user_name, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        die('<div style="color: red;">⚠️ Database connection error: ' . $e->getMessage() . '</div>');
+    }
 ?>
